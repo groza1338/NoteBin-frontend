1c2f1bbe761cc4e06a41a00b576a396bf2be1200
groza_1337 <groza_1337@mail.ru>
2025-01-21 16:53:58 +0300

    добавил все роуты

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import CreateNotePage from "./pages/CreateNotePage";
+import MyNotesPage from "./pages/MyNotesPage";
+import ViewNotePage from "./pages/ViewNotePage";
 
 function App() {
     return (
         <Router>
             <Routes>
+                <Route path="/" element={<HomePage />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
-                <Route path="*" element={<LoginPage />} /> {/* Перенаправление на вход */}
+                <Route path="/create" element={<CreateNotePage />} />
+                <Route path="/my-notes" element={<MyNotesPage />} />
+                <Route path="/note/:noteId" element={<ViewNotePage />} />
             </Routes>
         </Router>
     );
