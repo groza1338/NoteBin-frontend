6750fe8b88bd92a952886483f66e453bd776f689
groza_1337 <groza_1337@mail.ru>
2025-01-29 18:33:04 +0300

    импорты подправил

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { authAPI } from "../api";
 
 const HomePage = () => {
     const [noteId, setNoteId] = useState("");
